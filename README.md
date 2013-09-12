3b9003bc52c720c717601b527f046301b48ca677
Darius Kazemi <darius.kazemi@gmail.com>
2013-09-12 12:14:47 -0400

    first

---
+++ README.md
@@ -0,0 +1,27 @@
+# wordfilter
+
+A small module meant for use in text generators that lets you filter strings for bad words.
+
+## Getting Started
+Install the module with: `npm install wordfilter`
+
+```javascript
+var wordfilter = require('wordfilter');
+wordfilter.blacklisted('does this string have a bad word in it?'); // "false"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Darius Kazemi  
+Licensed under the MIT license.
---
+++ test/wordfilter_test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var wordfilter = require('../lib/wordfilter.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['awesome'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'detects bad words in a string': function(test) {
+    test.expect(3);
+    // tests here
+    test.equal(typeof(wordfilter), 'object', 'should return an object');
+    test.equal(wordfilter.blacklisted('this string contains the word skank'), true, 'should be true');
+    test.equal(wordfilter.blacklisted('this string is clean!'), false, 'should be false');
+    test.done();
+  },
+};
