ee4936160d12b5a3a113a1890820fac3f9583054
hugovk <hugovk@users.noreply.github.com>
2015-03-15 21:16:27 +0200

    flake8

--- lib/wordfilter.py
+++ lib/wordfilter.py
@@ -2,23 +2,23 @@ import os
 import json
 
 class Wordfilter:
-  def __init__(self):
-    # json is in same directory as this class, given by __location__.
-    __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    with open(os.path.join(__location__, 'badwords.json')) as f:
-      self.blacklist = json.loads(f.read())
+    def __init__(self):
+        # json is in same directory as this class, given by __location__.
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        with open(os.path.join(__location__, 'badwords.json')) as f:
+            self.blacklist = json.loads(f.read())
 
-  def blacklisted(self, string):
-    test_string = string.lower()
-    for badword in self.blacklist:
-      if test_string.find(badword) != -1:
-        return True
+    def blacklisted(self, string):
+        test_string = string.lower()
+        for badword in self.blacklist:
+            if test_string.find(badword) != -1:
+                return True
 
-    return False
+        return False
 
-  def addWords(self, words):
-    self.blacklist.extend([word.lower() for word in words])
+    def addWords(self, words):
+        self.blacklist.extend([word.lower() for word in words])
 
-  def clearList(self):
-    self.blacklist = []
+    def clearList(self):
+        self.blacklist = []
