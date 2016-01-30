ff21cee316a5494b8a1d9cd2b14ecf6adc3a686e
Eli Brody <ewokker@gmail.com>
2016-01-31 00:07:58 +0200

    Replace tabs with spaces.

--- lib/wordfilter.rb
+++ lib/wordfilter.rb
@@ -1,48 +1,48 @@
 require 'json'
 
 module Wordfilter
-	BadWordsFileName = File.dirname(__FILE__) + "/badwords.json"
-	@@blacklist = nil
-	
-	def self.init_first_time
-		return if(!@@blacklist.nil?)
-		self.init
-	end
-	
-	def self.init
-		badwords_file = File.read(BadWordsFileName)
-		@@blacklist = JSON.parse(badwords_file)
-		@@blacklist.map!(&:downcase)
-		return
-	end
-	
-	def self.blacklisted? string
-		self.init_first_time
-		
-		string_to_test = string.downcase
-		
-		@@blacklist.each{|word|
-			return true if string_to_test.include? word
-		}
-		
-		return false
-	end
-	
-	def self.add_words words
-		self.init_first_time
-		words.map!(&:downcase)
-		@@blacklist += words
-		return
-	end
+    BadWordsFileName = File.dirname(__FILE__) + "/badwords.json"
+    @@blacklist = nil
 
-	def self.remove_word word
-		self.init_first_time
-		@@blacklist.delete word.downcase
-		return
-	end
-	
-	def self.clear_list
-		@@blacklist = []
-		return
-	end
+    def self.init_first_time
+        return if(!@@blacklist.nil?)
+        self.init
+    end
+
+    def self.init
+        badwords_file = File.read(BadWordsFileName)
+        @@blacklist = JSON.parse(badwords_file)
+        @@blacklist.map!(&:downcase)
+        return
+    end
+
+    def self.blacklisted? string
+        self.init_first_time
+
+        string_to_test = string.downcase
+
+        @@blacklist.each{|word|
+            return true if string_to_test.include? word
+        }
+
+        return false
+    end
+
+    def self.add_words words
+        self.init_first_time
+        words.map!(&:downcase)
+        @@blacklist += words
+        return
+    end
+
+    def self.remove_word word
+        self.init_first_time
+        @@blacklist.delete word.downcase
+        return
+    end
+
+    def self.clear_list
+        @@blacklist = []
+        return
+    end
 end
