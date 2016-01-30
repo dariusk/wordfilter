3e8651b6b6f13d049f059431e98283ef0ba6ea65
Eli Brody <ewokker@gmail.com>
2016-01-30 19:54:32 +0200

    Update Ruby library to match latest javascript version. Minor code fixes.

--- lib/wordfilter.rb
+++ lib/wordfilter.rb
@@ -12,13 +12,13 @@ module Wordfilter
 	def self.init
 		badwords_file = File.read(BadWordsFileName);
 		@@blacklist = JSON.parse(badwords_file);
+		@@blacklist.map!(&:downcase)
 	end
 	
 	def self.blacklisted? string
 		self.init_first_time
 		
 		string_to_test = string.downcase
-		@@blacklist.map!{|badword| badword.downcase}
 		
 		@@blacklist.each{|word|
 			return true if string_to_test.include? word
@@ -29,7 +29,13 @@ module Wordfilter
 	
 	def self.add_words words
 		self.init_first_time
+		words.map!(&:downcase)
 		@@blacklist += words
+	end
+
+	def self.remove_word word
+		self.init_first_time
+		@@blacklist.delete word.downcase
 	end
 	
 	def self.clear_list
